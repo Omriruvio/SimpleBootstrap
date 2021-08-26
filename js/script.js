9dfcc8a5993c6221c3a9ab408056103d356545f8
omriruvio <omriruvio@gmail.com>
2021-08-26 15:20:39 +0300

    improved collapsable nav and box behavior fixed backtotop bug related to uncollapsing of menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready( function () {
 
-    $(".row div, h1").click(function (event) {
+
+    $("div.backtotop").addClass("d-sm-none");
+    // $(window).resize( function() {
+    //     var screenWidth = window.innerWidth;
+    //     if (screenWidth > 768) {
+    //         $("div.backtotop").addClass("d-sm-none");
+    //     }
+    // });
+
+
+    $("#MainSection, h1").click(function (event) {
        var screenWidth = window.innerWidth;
        if (screenWidth < 768) {
            $("#NavbarItems").collapse('hide');
